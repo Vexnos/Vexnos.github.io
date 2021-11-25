c2227eb561b27095ff454da31eaeb0cb55735131
Vexnos <88638499+Vexnos@users.noreply.github.com>
2021-11-25 14:52:20 +1300

    Mobile support

--- js/main.js
+++ js/main.js
@@ -4,6 +4,15 @@ Project: The website of Vexnos
 Author: Daniel Lagesse
 Date: 2021-11-23
 ----------------------------*/
+/*---- Hamburger Menu ----*/
+const toggle = document.querySelector(".toggle-btn");
+const nav = document.querySelector(".links");
+
+toggle.addEventListener('click', () => {
+  nav.classList.toggle('clicked');
+});
+
+/*---- Loading Screen ----*/
 window.addEventListener("load", function(){
   const loader = document.querySelector(".loader");
   const content = document.querySelector(".content");
